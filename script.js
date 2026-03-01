19b16218e9c14d59b2409a016387cbb303c1522c
Pranav-Shivale <pranavshivale69@gmail.com>
2026-03-01 19:53:48 +0530

    Add javascript logic
    Event Listeners for number, operator, equal, and clear buttons.
    - Number button: Takes input as a string and stores it in a temporary variable.
    - Operator button: The operator is selected, also the contents of temporary variable are transferred to number1. If the result exists they're transferred to number1.
    - Equal button: Does main calculation after the contents of temporary variable are put into number2.
    - Clear button: Clears the display and variables are set to zero.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let number1;
 let operator;
 let number2;
+let tempNum = "";
+let result;
+
+let numButtons = document.querySelectorAll(".num-btn");
+let opButtons = document.querySelectorAll(".op-btn");
+let equalButton = document.querySelector(".equal-btn");
+let clearButton = document.querySelector(".clear-btn");
+let display = document.querySelector(".display");
 
 function add(a, b) {
   return a+b;
@@ -11,27 +19,61 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-  return a*b;
+  return +(a*b).toFixed(10);
 }
 
 function divide(a, b) {
-  return a/b;
+  return +(a/b).toFixed(10);
 }
 
 function operate(a, b, op) {
   switch(op) {
     case "+":
-      add(a, b);
-      break;
+      return add(a, b);
     case "-":
-      subtract(a, b);
-      break;
+      return subtract(a, b);
     case "*":
-      multiply(a, b);
-      break;
+      return multiply(a, b);
     case "/":
-      divide(a, b);
-    default:
-      console.log("Invalid Operator.");
+      if(b === 0) {
+        alert("Division by zero not possible!!!");
+        return 0;
+      }
+      return divide(a, b);
   }
 } 
+
+numButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    tempNum += button.textContent;
+    display.textContent = tempNum;
+    console.log("Number clicked. tempNum: " ,tempNum);
+  });
+});
+
+opButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    operator = button.textContent;
+    if(result) number1 = result;
+    else number1 = +tempNum;
+    tempNum = "";
+    console.log("OP button number1: ",number1);
+  });
+});
+
+equalButton.addEventListener("click", () => {
+  number2 = +tempNum;
+  result = operate(number1, number2, operator);
+  display.textContent = result; 
+  tempNum = "";
+  console.log("Equal button. number2: ",number2, "result: ",result);
+});
+
+clearButton.addEventListener("click", () => {
+  tempNum = "";
+  number1 = 0;
+  number2 = 0;
+  result = 0;
+  display.textContent = 0;
+  console.clear();
+});
